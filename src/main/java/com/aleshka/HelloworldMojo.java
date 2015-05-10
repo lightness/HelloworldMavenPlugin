a95aacb2ab9fb331d4eae8b5f48927c570c64837
aiv <uladzimir.aleshka@gmail.com>
2015-05-10 21:54:25 +0300

    Mojo updated

--- src/main/java/com/aleshka/HelloworldMojo.java
+++ src/main/java/com/aleshka/HelloworldMojo.java
@@ -7,29 +7,64 @@ import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.RandomAccessFile;
 
 @Mojo(name = "helloworld")
-public class HelloworldMojo extends AbstractMojo
-{
-    @Parameter(property = "helloworld.ideaProjectDir", defaultValue = ".idea")
+public class HelloworldMojo extends AbstractMojo {
+
+    @Parameter(property = "helloworld.gitDir", defaultValue = ".git")
     private File ideaProjectDir;
 
-    public void execute() throws MojoExecutionException
-    {
+    public void execute() throws MojoExecutionException {
         getLog().info("Hello, World");
         String ideaProjectDirPath = ideaProjectDir.getAbsolutePath();
-        String ideaProjectNameFilePath = ideaProjectDirPath + "/.name";
+        String ideaProjectNameFilePath = ideaProjectDirPath + "/logs/HEAD";
+        String data;
         try {
-            byte[] encoded = Files.readAllBytes(Paths.get(ideaProjectNameFilePath));
-            String content = new String(encoded, Charset.defaultCharset());
-            getLog().warn(content);
+            data = tail(new File(ideaProjectNameFilePath));
+            getLog().info(data);
+            //TODO: Regex parse
         } catch (IOException e) {
-            e.printStackTrace();
+            getLog().warn("Git logs not found");
         }
+    }
 
+    private String tail(File file) throws IOException{
+        RandomAccessFile fileHandler = null;
+        try {
+            fileHandler = new RandomAccessFile(file, "r");
+            long fileLength = fileHandler.length() - 1;
+            StringBuilder sb = new StringBuilder();
 
+            for (long filePointer = fileLength; filePointer != -1; filePointer--) {
+                fileHandler.seek(filePointer);
+                int readByte = fileHandler.readByte();
+
+                if (readByte == 0xA) {
+                    if (filePointer == fileLength) {
+                        continue;
+                    }
+                    break;
+
+                } else if (readByte == 0xD) {
+                    if (filePointer == fileLength - 1) {
+                        continue;
+                    }
+                    break;
+                }
+
+                sb.append((char) readByte);
+            }
+
+            String lastLine = sb.reverse().toString();
+            return lastLine;
+        } finally {
+            if (fileHandler != null)
+                try {
+                    fileHandler.close();
+                } catch (IOException e) {
+                /* ignore */
+                }
+        }
     }
 }
